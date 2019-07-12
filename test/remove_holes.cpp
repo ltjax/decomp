4325382ee1546c52470e7cf220946c8d6081a11b
ltjax <marius.elvert@googlemail.com>
2019-07-12 15:39:59 +0200

    Cleaner test case

--- test/remove_holes.cpp
+++ test/remove_holes.cpp
@@ -155,22 +155,16 @@ TEST_CASE("Issue #1")
 //        { 437, 438, 439, 440, 441, 442 },
 //    };
 
-    std::vector<IndexList> works = {
-            //{303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320},
-//            {321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332,
-//                                                                         333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343},
-//            {344, 345, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355},
-            {356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374},
-//            {375, 376, 377, 378, 379, 380, 381, 382, 383, 384, 385, 386},
-    };
 
     // This one crashes
-    IndexList last = { 387, 388, 389, 390, 391, 392 };
+    IndexList holeThatGeneratedWeirdConnection = {356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374};
+    IndexList holeThatTriggersTheCrash = { 387, 388, 389, 390, 391, 392 };
 
     // Try not to crash
+    auto good = removeHoles(pointList, outer, {holeThatGeneratedWeirdConnection});
+    //std::ofstream svg("error.svg");
+    //svg::writePolygon(svg, pointList, good, {holeThatTriggersTheCrash});
+    auto bad = removeHoles(pointList, good, {holeThatTriggersTheCrash});
 
-    auto good = removeHoles(pointList, outer, works);
-    //std::ofstream file("good.svg");
-    //svg::writePolygon(file, pointList, good, {last});
-    auto bad = removeHoles(pointList, good, {last});
+    REQUIRE(true);
 }
