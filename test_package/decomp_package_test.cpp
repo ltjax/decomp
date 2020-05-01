2913b476f800807203980544b4091ede53e9200e
ltjax <marius.elvert@googlemail.com>
2020-05-01 20:25:50 +0200

    Fix test_package

--- test_package/decomp_package_test.cpp
+++ test_package/decomp_package_test.cpp
@@ -1,6 +1,7 @@
 #include <decomp/convex_decomposition.hpp>
 
-int main() {
-  auto _ = decomp::hertelMehlhorn({}, {});
-  return 0;
+int main()
+{
+    auto _ = decomp::hertelMehlhorn({}, {}, {});
+    return 0;
 }
