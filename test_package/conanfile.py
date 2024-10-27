868bdf1278fd954310680fa3a0de97bbcf6b54a2
Marius Elvert <marius.elvert@googlemail.com>
2024-10-27 23:25:07 +0100

    Port to conan 2

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,11 +1,17 @@
 import os
 
-from conans import ConanFile, CMake, tools
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
 
 
 class DecompTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "CMakeDeps", "CMakeToolchain"
+    
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
@@ -13,8 +19,11 @@ class DecompTestConan(ConanFile):
         # in "test_package"
         cmake.configure()
         cmake.build()
+        
+    def layout(self):
+        cmake_layout(self)
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            os.chdir("bin")
-            self.run(".%sdecomp_package_test" % os.sep)
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "decomp_package_test")
+            self.run(cmd, env="conanrun")
